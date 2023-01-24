bb32b29d160a019ac213537d04c6809806f4130f
hwi.contact.here@gmail.com <bnmbnmm@ajou.ac.kr>
2023-01-24 22:33:03 +0900

    Update Authentication

---
+++ lib/widgets/auth_widget.dart
@@ -0,0 +1,75 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthFunc extends StatelessWidget {
+  const AuthFunc({
+    super.key,
+    required this.loggedIn,
+    required this.signOut,
+  });
+
+  final bool loggedIn;
+  final void Function() signOut;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 24, bottom: 8),
+          child: OutlinedButton(
+              onPressed: () {
+                !loggedIn ? context.push('/sign-in') : signOut();
+              },
+              child: !loggedIn ? const Text('Login') : const Text('Logout')),
+        ),
+        Visibility(
+            visible: loggedIn,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 24, bottom: 8),
+              child: OutlinedButton(
+                  onPressed: () {
+                    context.push('/profile');
+                  },
+                  child: const Text('Profile')),
+            ))
+      ],
+    );
+  }
+}
+
+class googleLogin extends StatelessWidget {
+  const googleLogin({Key? key}) : super(key: key);
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: signInWithGoogle,
+      child: const Text("Google Login"),
+    );
+  }
+}
